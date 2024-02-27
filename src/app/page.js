c5f47c30da48ba7d5c65df7aa04e74f6365cc5d4
Daniel Carvalho <daniel.carvalhoshrn@gmail.com>
2024-02-27 13:05:32 -0300

    :sparkles: :heavy_plus_sign: :dizzy: Header Section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import HeaderSection from "./components/HeaderSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen">
-      <h1>Portfolio Website</h1>
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <div class="container mx-auto px-12 py-4">
+        <HeaderSection />
+      </div>
     </main>
   );
 }
